1d1e3df7ca0d89273ba0b13443da15994eb7996a
kalelpida <cordip@outlook.fr>
2022-12-22 15:00:29 +0100

    mise à jour script install dependencies

--- hypatia_install_dependencies.sh
+++ hypatia_install_dependencies.sh
@@ -2,16 +2,16 @@
 echo "Hypatia: installing dependencies"
 echo ""
 echo "It is highly recommend you use a recent Linux operating system (e.g., Ubuntu 20 or higher)."
-echo "Python version 3.8+ is required."
+echo "Python version 3.9+ is required."
 echo ""
 
 # General
 sudo apt-get update || exit 1
 
-#check python3.8+
-versionPython=$(python --version)
-versionPython=${versionPython:6:4}
-if [[ "${versionPython:0:1}" < 3 ]];then
+#check python3.9+
+versionString=$(python --version)
+versionArray=(`sed -n 's/\./ /gp' <<< ${versionString:7}`)
+if (( ${versionArray[1]} < 3 ));then
 echo "we recommend installing python-is-python3 module if you don't need python2\
 	otherwise you can replace 'python' by 'python3' everywhere "
 echo "proceed to python-is-python3 installation ? [y/n]"
@@ -22,8 +22,8 @@ else echo "python is not replaced by python3, quit"; exit 1
 fi
 fi
 
-if (( "${versionPython:2}" < "8" )); then
-echo "please upgrade python to python 3.8+"
+if (( ${versionArray[2]} < 10 )); then
+echo "please upgrade python to python 3.9+"
 exit 1
 fi
 
