6acdb7d15ca9feb75f62e136c6ddfd7d0fd7bca2
kalelpida <cordip@outlook.fr>
2022-12-22 11:37:55 +0100

    utilisateurs dans les bornes de la constellation commit2

---
+++ papier2/satellite_networks_state/input_data/constants.py
@@ -0,0 +1,5 @@
+# WGS72 value; taken from https://geographiclib.sourceforge.io/html/NET/NETGeographicLib_8h_source.html
+EARTH_RADIUS= 6378135.0
+MU_EARTH= 3.98574405e+14
+SECONDS_SIDEREAL_DAY= 86164
+RADIO_K_FACTOR = 1.33 # The K-factor varies a lot according to temperature, humidity
